7ab50446a1496a975e6390ac7ba38167f6513c3a
Peter Szabados <kangirigungi@gmail.com>
2015-12-03 22:22:57 +0100

    Can upload basic program

--- arduino/test.cpp
+++ arduino/test.cpp
@@ -4,6 +4,8 @@ int ledPin = 2;                 // LED connected to digital pin 13
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial.println("START");
   pinMode(ledPin, OUTPUT);      // sets the digital pin as output
 }
 
