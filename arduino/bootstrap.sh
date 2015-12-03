72db93c2b3593d8a8d1d8355c34797bb14aad02c
Peter Szabados <kangirigungi@gmail.com>
2015-12-03 20:59:09 +0100

    Compiles and links

--- arduino/bootstrap.sh
+++ arduino/bootstrap.sh
@@ -3,6 +3,6 @@
 set -e
 source_dir="$ESP_DIR/cores/esp8266"
 core_dir="$(dirname "$(which "$0")")/core"
-cp -t "$core_dir" "$source_dir"/*.S "$source_dir"/*.c
+cp -t "$core_dir" "$source_dir"/*.S "$source_dir"/*.c "$source_dir"/*.cpp
 tup "${core_dir}/core.a"
 
