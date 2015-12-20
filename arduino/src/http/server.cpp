67bc69fca73bbfc21685a5384c5b50fd8c464e6d
Peter Szabados <kangirigungi@gmail.com>
2015-12-20 17:10:03 +0100

    Print proper IP address to json

--- arduino/src/http/server.cpp
+++ arduino/src/http/server.cpp
@@ -14,9 +14,12 @@ namespace http {
 
 String getContent(const String& path) {
     if (path == "/") {
+        IPAddress ip = WiFi.localIP();
+        String ipString = String(ip[0]) + '.' + String(ip[1]) + '.' +
+                String(ip[2]) + '.' + String(ip[3]);
         return "{ \"device\": { "
                 "\"name\": \"" + String(device::name) + "\", "
-                "\"ip\": \"" + String(WiFi.localIP()) + "\", "
+                "\"ip\": \"" + ipString + "\", "
                 "\"memory\": " + String(system_get_free_heap_size()) + ", "
                 "\"rssi\": " + String(WiFi.RSSI()) + "}}";
     } else {
