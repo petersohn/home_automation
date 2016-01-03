b79b9ce2b5c04df93ca8cd5d7ed705ff28923e4f
Peter Szabados <kangirigungi@gmail.com>
2016-01-03 09:36:14 +0100

    Print real directory to compile_commands.json

--- print_json.sh
+++ print_json.sh
@@ -1,10 +1,37 @@
 #!/usr/bin/env bash
 
+eliminateTupVariant() {
+    topDir="$PWD"
+    while [ "$PWD" != / -a ! -e ../.tup -a ! -e .tup ]; do
+        cd ..
+    done
+
+    if [ -e tup.config ]; then
+        configDir=$PWD
+        cd ..
+        echo "$PWD${topDir#$configDir}"
+    else
+        echo "$topDir"
+    fi
+}
+
+findRealDirectory() {
+    pushd . >/dev/null
+    if pwd | grep --quiet /.tup/mnt/@tupjob-; then
+        # get out from the tup virtual filesystem
+        cd "/${PWD#*/.tup/mnt/@tupjob-*/}"
+
+        # get out from the tup variant directory (if any)
+        cd "$(eliminateTupVariant)"
+    fi
+}
+
 file="$1"
 shift
+
 cat >"$(basename $file).compile.json" <<_EOF_
 [{
-    "directory": "$PWD",
+    "directory": "$(findRealDirectory; pwd)",
     "command": "$*",
     "file": "$file"
 }]
