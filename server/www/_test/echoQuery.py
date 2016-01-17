e2609112ce7cf6ee628c3e3703038c9e19535587
Peter Szabados <kangirigungi@gmail.com>
2016-01-17 18:44:19 +0100

    Remove unnecessary test from web server

--- server/www/_test/echoQuery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-
-import urllib
-
-def run(environ, senderQueue, response):
-    response.headers = [('Content-Type', 'text/plain')]
-    return urllib.parse.unquote(environ["QUERY_STRING"])
-
