db91dca9f7313386130e210cfe691e67db3df9a5
Peter Szabados <kangirigungi@gmail.com>
2016-03-12 11:45:38 +0100

    Clean up old stuff

--- server/test/tests.py
+++ server/test/tests.py
@@ -1,36 +1,6 @@
 #!/usr/bin/env python3
 
 import senderTest
-import test_globals
-
-import psycopg2
-
-
-def setUpModule():
-    connection = psycopg2.connect(test_globals.connectString)
-    with open(test_globals.sqlDirectory + "/tables.sql") as script:
-        connection.cursor().execute(script.read())
-        connection.commit()
-
-
-# class SessionTest(databaseTest.SessionTest):
-    # pass
-
-
-# class ExecuteTransactionallyTest(databaseTest.ExecuteTransactionallyTest):
-    # pass
-
-
-# class VariablesTest(databaseTest.VariablesTest):
-    # pass
-
-
-# class DevicesTest(databaseTest.DevicesTest):
-    # pass
-
-
-# class LoggerTest(databaseTest.DevicesTest):
-    # pass
 
 
 class RequestTest(senderTest.RequestTest):
@@ -43,4 +13,3 @@ class ClearDeviceTest(senderTest.ClearDeviceTest):
 
 class ConnectionTest(senderTest.ConnectionTest):
     pass
-
