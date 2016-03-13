6102fd2c326ef18f0f2b01010d9a1d248b59d5da
Peter Szabados <kangirigungi@gmail.com>
2016-03-13 12:42:08 +0100

    Make server work with fastcgi

--- server/home_automation/home_automation/wsgi.py
+++ server/home_automation/home_automation/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for home_automation project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
+#!/usr/bin/env python3
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from flipflop2 import WSGIServer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home_automation.settings")
 
+scriptDirectory = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(scriptDirectory + "/..")
+
 application = get_wsgi_application()
+
+
+if __name__ == '__main__':
+    server = WSGIServer(application)
+    server.run()
