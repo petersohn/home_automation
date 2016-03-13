08a70a8f32bb7c7a06a0a11c596a4a63ec95e406
Peter Szabados <kangirigungi@gmail.com>
2016-03-13 22:00:57 +0100

    Make admin work

--- server/home_automation/home_automation/wsgi.py
+++ server/home_automation/home_automation/wsgi.py
@@ -3,16 +3,26 @@
 import os
 import sys
 
+scriptDirectory = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(scriptDirectory + "/..")
+sys.path.append(scriptDirectory)
+
 from django.core.wsgi import get_wsgi_application
 from flipflop2 import WSGIServer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home_automation.settings")
 
-scriptDirectory = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(scriptDirectory + "/..")
+my_application = get_wsgi_application()
 
-application = get_wsgi_application()
 
+def application(environ, start_response):
+    # maxlen = max([len(key) for key, value in environ.items()])
+    # for k, v in sorted(environ.items()):
+        # sys.stderr.write(
+            # "{{0:{}}}".format(maxlen + 1).format(str(k)) + " --> " +
+            # str(v) + "\n")
+    environ["SCRIPT_NAME"] = ""
+    return my_application(environ, start_response)
 
 if __name__ == '__main__':
     server = WSGIServer(application)
