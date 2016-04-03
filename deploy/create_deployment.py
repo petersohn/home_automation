5020c0e5b7a60a4a165c2776dba4d8342c12d43b
Peter Szabados <kangirigungi@gmail.com>
2016-04-03 13:01:41 +0200

    Implement downloading of jquery-ui

--- deploy/create_deployment.py
+++ deploy/create_deployment.py
@@ -3,11 +3,61 @@
 import argparse
 import git
 import os
+import sys
 import tarfile
+import tempfile
+from urllib.request import Request, urlopen
+import zipfile
+
+
+STATIC_FILES_DIR = "server/home_automation/home/static/home"
+
+
+def download_file(url):
+    sys.stderr.write("Downloading " + url + "\n")
+    with urlopen(Request(url, headers={"User-Agent": "Foobar"})) as response:
+        if response.status != 200:
+            raise RuntimeError("Got HTTP error: " + response.msg)
+        length = int(response.getheader("Content-Length"))
+        amount = 1024 * 4
+        target = tempfile.TemporaryFile()
+        downloaded = 0
+        if length is not None:
+            sys.stderr.write("0.0%\r")
+        try:
+            while True:
+                data = response.read(amount)
+                if len(data) == 0:
+                    target.seek(0)
+                    return target
+                target.write(data)
+                downloaded += len(data)
+                if length is not None:
+                    sys.stderr.write(
+                        "{:.1f}%\r".format(downloaded / length * 100))
+                else:
+                    sys.stderr.write(".")
+        finally:
+            sys.stderr.write("\n")
+
+
+def process_zip(name, target_dir, new_name):
+    target_path = target_dir + "/" + new_name
+    if not os.path.exists(target_path):
+        with download_file(
+                "http://jqueryui.com/resources/download/" +
+                name + ".zip") as f:
+            with zipfile.ZipFile(f) as zip:
+                zip.extractall(target_dir)
+                if new_name is not None:
+                    os.rename(target_dir + "/" + name, target_path)
 
 
 def download_dependencies():
-    pass
+    global STATIC_FILES_DIR
+    process_zip("jquery-ui-1.11.4", STATIC_FILES_DIR, "jquery-ui")
+    process_zip(
+        "jquery-ui-themes-1.11.4", STATIC_FILES_DIR, "jquery-ui-themes")
 
 
 def create_archive(filename):
