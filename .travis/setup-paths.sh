2edb3dff980d74082a158fae071ad4ec77ed1ff0
Peter Szabados <kangirigungi@gmail.com>
2016-04-30 21:49:05 +0200

    Make travis environment work

--- .travis/setup-paths.sh
+++ .travis/setup-paths.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 
+create_link() {
+    file="$1"
+    source="$VIRTUAL_ENV/$file"
+    target="/usr/local/$file"
+    rm -rf "$target"
+    cp -r "$source" "$target"
+}
+
+create_links() {
+    for f in $(eval "echo $*"); do
+        create_link "${f#$VIRTUAL_ENV}"
+    done
+}
+
 set -ex
 
-rm -rf /usr/local/lib
-ln -s $VIRTUAL_ENV/lib /usr/local/lib
-rm -rf /usr/local/bin
-ln -s $VIRTUAL_ENV/bin /usr/local/bin
+echo $VIRTUAL_ENV
 
+create_links '$VIRTUAL_ENV/lib/python*'
+create_links '$VIRTUAL_ENV/bin/python*'
+create_links '$VIRTUAL_ENV/bin/pip*'
 
+for file in /usr/local/bin/pip*; do
+    version="${file##*pip}"
+    sed -i "s|^#!.*$|#!/usr/local/bin/python${version}|" "$file"
+done
