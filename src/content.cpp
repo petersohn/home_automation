16b7518ff4057fd0795dcbbc66289a975d95f748
Peter Szabados <kangirigungi@gmail.com>
2017-06-20 22:21:35 +0200

    Use ArduinoJson library for REST API generation

--- src/content.cpp
+++ src/content.cpp
@@ -5,6 +5,7 @@
 #include "string.hpp"
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 
 #include <algorithm>
@@ -15,47 +16,56 @@ extern "C" {
 
 namespace {
 
-String getDeviceInfo() {
+JsonObject& getDeviceInfo(DynamicJsonBuffer& buffer) {
     IPAddress ip = WiFi.localIP();
     String ipString = String(ip[0]) + '.' + String(ip[1]) + '.' +
             String(ip[2]) + '.' + String(ip[3]);
-    return "{ "
-        "\"name\": \"" + String(deviceConfig.name) + "\", "
-        "\"ip\": \"" + ipString + "\", "
-        "\"port\": 80, "
-        "\"memory\": " + String(system_get_free_heap_size()) + ", "
-        "\"version\": 2, "
-        "\"uptime\": " + String(millis()) + ", "
-        "\"rssi\": " + String(WiFi.RSSI()) + " }";
+    JsonObject& result = buffer.createObject();
+    result["name"] = deviceConfig.name;
+    result["ip"] = ipString;
+    result["port"] = 80;
+    result["memory"] = system_get_free_heap_size();
+    result["version"] = 2;
+    result["uptime"] = millis();
+    result["rssi"] = WiFi.RSSI();
+    return result;
 }
 
-String handleInterface(const InterfaceConfig& interface, const String& path) {
-    auto result = interface.interface->answer(path);
-    if (result.success) {
-        return "{ "
-            "\"name\": \"" + String(interface.name) + "\", "
-            "\"value\": " + result.value + " }";
+JsonObject* handleInterface(DynamicJsonBuffer& buffer,
+        const InterfaceConfig& interface, const String& path) {
+    auto answer = interface.interface->answer(path);
+    if (answer.success) {
+        JsonObject& result = buffer.createObject();
+        result["name"] = interface.name;
+        result["value"] = answer.value;
+        return &result;
     }
-    DEBUGLN(result.value);
-    return "";
+    DEBUGLN(answer.value);
+    return nullptr;
 }
 
-String getFullStatus() {
-    tools::Join interfaceData{", "};
+JsonObject& getFullStatus(DynamicJsonBuffer& buffer) {
+    JsonObject& result = buffer.createObject();
+    result["device"] = getDeviceInfo(buffer);
+
+    JsonArray& interfaceData = buffer.createArray();
+    result["interfaces"] = interfaceData;
+
     for (const InterfaceConfig& pin : deviceConfig.interfaces) {
-        interfaceData.add(handleInterface(pin, ""));
+        JsonObject* object = handleInterface(buffer, pin, "");
+        if (object) {
+            interfaceData.add(*object);
+        }
     }
-    String result = "{ \"device\": " + getDeviceInfo() + ", "
-        "\"pins\": [ " + interfaceData.get() + " ]";
-    return result + " }";
+
+    return result;
 }
 
-} // unnamed namespace
-
-String getContent(const String& method, const String& path,
+JsonObject* doGetContent(DynamicJsonBuffer& buffer,
+        const String& method, const String& path,
         const String& content) {
     if (!path.startsWith("/")) {
-        return "";
+        return nullptr;
     }
 
     int interfaceNameEnd = path.indexOf('/', 1);
@@ -65,7 +75,7 @@ String getContent(const String& method, const String& path,
 
     String interfaceName = path.substring(1, interfaceNameEnd);
     if (interfaceName.length() == 0) {
-        return getFullStatus();
+        return &getFullStatus(buffer);
     }
 
     auto interface = std::find_if(
@@ -74,10 +84,26 @@ String getContent(const String& method, const String& path,
                 return interfaceName == interface.name;
             });
     if (interface == deviceConfig.interfaces.end()) {
-        return "";
+        return nullptr;
     }
 
     const String& value = (method == "POST") ? content
             : path.substring(interfaceNameEnd + 1);
-    return handleInterface(*interface, value);
+    return handleInterface(buffer, *interface, value);
+}
+
+} // unnamed namespace
+
+String getContent(const String& method, const String& path,
+        const String& content) {
+    DynamicJsonBuffer buffer{512};
+    JsonObject* result = doGetContent(buffer, method, path, content);
+
+    if (!result) {
+        return "";
+    }
+
+    String output;
+    result->printTo(output);
+    return output;
 }
