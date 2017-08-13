43898b17d6077a50089cf0f13322aa6be410169b
Peter Szabados <kangirigungi@gmail.com>
2017-08-13 15:09:18 +0200

    Use device name as host name

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
+#include <cstring>
 #include <limits>
 
 extern "C" {
@@ -17,12 +18,27 @@ namespace {
 constexpr unsigned long timeLimit =
         std::numeric_limits<unsigned long>::max() - 60000;
 
+void setDeviceName() {
+    static char* name = nullptr;
+    if (name) {
+        return;
+    }
+
+    // This function accepts a char*, and it is not documented if copies the
+    // name or not. To be safe, the string is made permanent.
+    name = new char[deviceConfig.name.length() + 5];
+    std::strcpy(name, "ESP_");
+    std::strcat(name, deviceConfig.name.c_str());
+    wifi_station_set_hostname(name);
+}
+
 } // unnamed namespace
 
 void setup()
 {
     WiFi.mode(WIFI_STA);
     initConfig();
+    setDeviceName();
 }
 
 void loop()
