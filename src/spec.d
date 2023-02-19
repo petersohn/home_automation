d7bb0e27236c0c03f47b31e84aa5d3f142a83a21
Peter Szabados <kangirigungi@gmail.com>
2023-02-19 15:14:19 +0100

    Update firmware

--- src/spec.d
+++ src/spec.d
@@ -1,2 +1,4 @@
 spec.o: \
- /home/petersohn/eclipse-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.cpp
+ /home/petersohn/Documents/sloeber-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.cpp \
+ /home/petersohn/eclipse/Sloeber/arduinoPlugin/packages/esp8266/hardware/esp8266/3.1.1/cores/esp8266/CommonHFile.h \
+ /home/petersohn/workspace/home_automation/src/Release/core/home_automation.globals.h
