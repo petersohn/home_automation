6baa8833d7a1b939da98ee66b222d7b8618a9ddf
Peter Szabados <kangirigungi@gmail.com>
2026-06-09 20:42:11 +0200

    Migrate stringTest to googletest

--- test/stringTest.cpp
+++ test/stringTest.cpp
@@ -1,290 +1,261 @@
-#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 
 #include "tools/string.hpp"
 
-BOOST_AUTO_TEST_SUITE(StringTest)
-
-BOOST_AUTO_TEST_SUITE(NextTokenTest)
-
-BOOST_AUTO_TEST_CASE(ReadTokensInString) {
+TEST(StringTest, NextTokenTest_ReadTokensInString) {
     const std::string s = "foo bar foobar baz";
     std::size_t position = 0;
 
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "foo");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "bar");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "foobar");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "baz");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "");
-    BOOST_TEST(position == s.size());
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "foo");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "bar");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "foobar");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "baz");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "");
+    EXPECT_EQ(position, s.size());
 }
 
-BOOST_AUTO_TEST_CASE(MultipleSeparatorsAtBeginning) {
+TEST(StringTest, NextTokenTest_MultipleSeparatorsAtBeginning) {
     const std::string s = "   foo bar";
     std::size_t position = 0;
 
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "foo");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "bar");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "");
-    BOOST_TEST(position == s.size());
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "foo");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "bar");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "");
+    EXPECT_EQ(position, s.size());
 }
 
-BOOST_AUTO_TEST_CASE(MultipleSeparatorsAtEnd) {
+TEST(StringTest, NextTokenTest_MultipleSeparatorsAtEnd) {
     const std::string s = "foobar foo   ";
     std::size_t position = 0;
 
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "foobar");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "foo");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "");
-    BOOST_TEST(position == s.size());
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "foobar");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "foo");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "");
+    EXPECT_EQ(position, s.size());
 }
 
-BOOST_AUTO_TEST_CASE(MultipleSeparatorsBetweenElements) {
+TEST(StringTest, NextTokenTest_MultipleSeparatorsBetweenElements) {
     const std::string s = "foobar  foo   baz barbar";
     std::size_t position = 0;
 
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "foobar");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "foo");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "baz");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "barbar");
-    BOOST_TEST(tools::nextToken(s, ' ', position) == "");
-    BOOST_TEST(position == s.size());
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "foobar");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "foo");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "baz");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "barbar");
+    EXPECT_EQ(tools::nextToken(s, ' ', position), "");
+    EXPECT_EQ(position, s.size());
 }
 
-BOOST_AUTO_TEST_CASE(DifferentSeparator) {
+TEST(StringTest, NextTokenTest_DifferentSeparator) {
     const std::string s = "foo#bar#foobar";
     std::size_t position = 0;
 
-    BOOST_TEST(tools::nextToken(s, '#', position) == "foo");
-    BOOST_TEST(tools::nextToken(s, '#', position) == "bar");
-    BOOST_TEST(tools::nextToken(s, '#', position) == "foobar");
-    BOOST_TEST(tools::nextToken(s, '#', position) == "");
-    BOOST_TEST(position == s.size());
+    EXPECT_EQ(tools::nextToken(s, '#', position), "foo");
+    EXPECT_EQ(tools::nextToken(s, '#', position), "bar");
+    EXPECT_EQ(tools::nextToken(s, '#', position), "foobar");
+    EXPECT_EQ(tools::nextToken(s, '#', position), "");
+    EXPECT_EQ(position, s.size());
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(JoinTest)
-
-BOOST_AUTO_TEST_CASE(SpaceAsSeparator) {
+TEST(StringTest, JoinTest_SpaceAsSeparator) {
     tools::Join join{" "};
     join.add("foo");
     join.add("bar");
     join.add("foobar");
     join.add("baz");
-    BOOST_TEST(join.get() == "foo bar foobar baz");
+    EXPECT_EQ(join.get(), "foo bar foobar baz");
 }
 
-BOOST_AUTO_TEST_CASE(LongerSeparator) {
+TEST(StringTest, JoinTest_LongerSeparator) {
     tools::Join join{"-->"};
     join.add("foo");
     join.add("barfoo");
     join.add("baz");
-    BOOST_TEST(join.get() == "foo-->barfoo-->baz");
+    EXPECT_EQ(join.get(), "foo-->barfoo-->baz");
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(SubstituteTest)
-
-BOOST_AUTO_TEST_CASE(SingleValue) {
+TEST(StringTest, SubstituteTest_SingleValue) {
     const std::vector<std::string> values{"baz"};
-    BOOST_TEST(tools::substitute("foo%1bar", values) == "foobazbar");
+    EXPECT_EQ(tools::substitute("foo%1bar", values), "foobazbar");
 }
 
-BOOST_AUTO_TEST_CASE(MultipleValues) {
+TEST(StringTest, SubstituteTest_MultipleValues) {
     const std::vector<std::string> values{"foo", "foobar"};
-    BOOST_TEST(tools::substitute("%1 %2", values) == "foo foobar");
+    EXPECT_EQ(tools::substitute("%1 %2", values), "foo foobar");
 }
 
-BOOST_AUTO_TEST_CASE(DifferentOrder) {
+TEST(StringTest, SubstituteTest_DifferentOrder) {
     const std::vector<std::string> values{"foo", "bar", "baz"};
-    BOOST_TEST(tools::substitute("%3+%1-%2", values) == "baz+foo-bar");
+    EXPECT_EQ(tools::substitute("%3+%1-%2", values), "baz+foo-bar");
 }
 
-BOOST_AUTO_TEST_CASE(MultipleOccurrances) {
+TEST(StringTest, SubstituteTest_MultipleOccurrances) {
     const std::vector<std::string> values{"foo", "bar"};
-    BOOST_TEST(tools::substitute("%1%1%2%2%1", values) == "foofoobarbarfoo");
+    EXPECT_EQ(tools::substitute("%1%1%2%2%1", values), "foofoobarbarfoo");
 }
 
-BOOST_AUTO_TEST_CASE(IgnoreInvalidReference) {
+TEST(StringTest, SubstituteTest_IgnoreInvalidReference) {
     const std::vector<std::string> values{"foo", "bar"};
-    BOOST_TEST(tools::substitute("%1 %3 %2", values) == "foo  bar");
+    EXPECT_EQ(tools::substitute("%1 %3 %2", values), "foo  bar");
 }
 
-BOOST_AUTO_TEST_CASE(LongerSequence) {
+TEST(StringTest, SubstituteTest_LongerSequence) {
     std::vector<std::string> values;
     for (int i = 0; i < 1000; ++i) {
         values.push_back("value" + std::to_string(i));
     }
-    BOOST_TEST(
-        tools::substitute("%124-->%654", values) == "value123-->value653");
+    EXPECT_EQ(tools::substitute("%124-->%654", values), "value123-->value653");
 }
 
-BOOST_AUTO_TEST_CASE(LiteralPercent) {
+TEST(StringTest, SubstituteTest_LiteralPercent) {
     const std::vector<std::string> values{"foo", "bar"};
-    BOOST_TEST(tools::substitute("%% %1%%%2%%", values) == "% foo%bar%");
+    EXPECT_EQ(tools::substitute("%% %1%%%2%%", values), "% foo%bar%");
 }
 
-BOOST_AUTO_TEST_CASE(IgnoreStrayPercent) {
+TEST(StringTest, SubstituteTest_IgnoreStrayPercent) {
     const std::vector<std::string> values{"foo", "bar"};
-    BOOST_TEST(tools::substitute("foo%bar", values) == "foobar");
+    EXPECT_EQ(tools::substitute("foo%bar", values), "foobar");
 }
 
-BOOST_AUTO_TEST_CASE(IgnoreZeroReference) {
+TEST(StringTest, SubstituteTest_IgnoreZeroReference) {
     const std::vector<std::string> values{"foo", "bar"};
-    BOOST_TEST(tools::substitute("%0 %1 %2", values) == " foo bar");
+    EXPECT_EQ(tools::substitute("%0 %1 %2", values), " foo bar");
 }
 
-BOOST_AUTO_TEST_CASE(IgnoreNegativeReference) {
+TEST(StringTest, SubstituteTest_IgnoreNegativeReference) {
     const std::vector<std::string> values{"foo", "bar"};
-    BOOST_TEST(tools::substitute("%-1", values) == "-1");
+    EXPECT_EQ(tools::substitute("%-1", values), "-1");
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(GetBoolValueTest)
-
-BOOST_AUTO_TEST_CASE(Zero) {
+TEST(StringTest, GetBoolValueTest_Zero) {
     bool result = false;
-    BOOST_TEST(tools::getBoolValue("0", result));
-    BOOST_TEST(result == false);
+    EXPECT_TRUE(tools::getBoolValue("0", result));
+    EXPECT_FALSE(result);
 }
 
-BOOST_AUTO_TEST_CASE(One) {
+TEST(StringTest, GetBoolValueTest_One) {
     bool result = false;
-    BOOST_TEST(tools::getBoolValue("1", result));
-    BOOST_TEST(result == true);
+    EXPECT_TRUE(tools::getBoolValue("1", result));
+    EXPECT_TRUE(result);
 }
 
-BOOST_AUTO_TEST_CASE(Off) {
+TEST(StringTest, GetBoolValueTest_Off) {
     bool result = false;
-    BOOST_TEST(tools::getBoolValue("off", result));
-    BOOST_TEST(result == false);
-    BOOST_TEST(tools::getBoolValue("Off", result));
-    BOOST_TEST(result == false);
-    BOOST_TEST(tools::getBoolValue("OFF", result));
-    BOOST_TEST(result == false);
-    BOOST_TEST(tools::getBoolValue("oFf", result));
-    BOOST_TEST(result == false);
+    EXPECT_TRUE(tools::getBoolValue("off", result));
+    EXPECT_FALSE(result);
+    EXPECT_TRUE(tools::getBoolValue("Off", result));
+    EXPECT_FALSE(result);
+    EXPECT_TRUE(tools::getBoolValue("OFF", result));
+    EXPECT_FALSE(result);
+    EXPECT_TRUE(tools::getBoolValue("oFf", result));
+    EXPECT_FALSE(result);
 }
 
-BOOST_AUTO_TEST_CASE(On) {
+TEST(StringTest, GetBoolValueTest_On) {
     bool result = false;
-    BOOST_TEST(tools::getBoolValue("on", result));
-    BOOST_TEST(result == true);
-    BOOST_TEST(tools::getBoolValue("On", result));
-    BOOST_TEST(result == true);
-    BOOST_TEST(tools::getBoolValue("oN", result));
-    BOOST_TEST(result == true);
-    BOOST_TEST(tools::getBoolValue("ON", result));
-    BOOST_TEST(result == true);
+    EXPECT_TRUE(tools::getBoolValue("on", result));
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(tools::getBoolValue("On", result));
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(tools::getBoolValue("oN", result));
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(tools::getBoolValue("ON", result));
+    EXPECT_TRUE(result);
 }
 
-BOOST_AUTO_TEST_CASE(False) {
+TEST(StringTest, GetBoolValueTest_False) {
     bool result = false;
-    BOOST_TEST(tools::getBoolValue("false", result));
-    BOOST_TEST(result == false);
-    BOOST_TEST(tools::getBoolValue("False", result));
-    BOOST_TEST(result == false);
-    BOOST_TEST(tools::getBoolValue("fAlSe", result));
-    BOOST_TEST(result == false);
-    BOOST_TEST(tools::getBoolValue("FALSE", result));
-    BOOST_TEST(result == false);
+    EXPECT_TRUE(tools::getBoolValue("false", result));
+    EXPECT_FALSE(result);
+    EXPECT_TRUE(tools::getBoolValue("False", result));
+    EXPECT_FALSE(result);
+    EXPECT_TRUE(tools::getBoolValue("fAlSe", result));
+    EXPECT_FALSE(result);
+    EXPECT_TRUE(tools::getBoolValue("FALSE", result));
+    EXPECT_FALSE(result);
 }
 
-BOOST_AUTO_TEST_CASE(True) {
+TEST(StringTest, GetBoolValueTest_True) {
     bool result = false;
-    BOOST_TEST(tools::getBoolValue("true", result));
-    BOOST_TEST(result == true);
-    BOOST_TEST(tools::getBoolValue("True", result));
-    BOOST_TEST(result == true);
-    BOOST_TEST(tools::getBoolValue("tRuE", result));
-    BOOST_TEST(result == true);
-    BOOST_TEST(tools::getBoolValue("TRUE", result));
-    BOOST_TEST(result == true);
+    EXPECT_TRUE(tools::getBoolValue("true", result));
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(tools::getBoolValue("True", result));
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(tools::getBoolValue("tRuE", result));
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(tools::getBoolValue("TRUE", result));
+    EXPECT_TRUE(result);
 }
 
-BOOST_AUTO_TEST_CASE(DifferentStartingValue) {
+TEST(StringTest, GetBoolValueTest_DifferentStartingValue) {
     bool result = true;
-    BOOST_TEST(tools::getBoolValue("1", result));
-    BOOST_TEST(result == true);
-    BOOST_TEST(tools::getBoolValue("0", result));
-    BOOST_TEST(result == false);
+    EXPECT_TRUE(tools::getBoolValue("1", result));
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(tools::getBoolValue("0", result));
+    EXPECT_FALSE(result);
 }
 
-BOOST_AUTO_TEST_CASE(InvalidValue) {
+TEST(StringTest, GetBoolValueTest_InvalidValue) {
     bool result = false;
-    BOOST_TEST(!tools::getBoolValue("foo", result));
-    BOOST_TEST(result == false);
+    EXPECT_FALSE(tools::getBoolValue("foo", result));
+    EXPECT_FALSE(result);
     result = true;
-    BOOST_TEST(!tools::getBoolValue("foo", result));
-    BOOST_TEST(result == true);
+    EXPECT_FALSE(tools::getBoolValue("foo", result));
+    EXPECT_TRUE(result);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(IntToStringTest)
-
-BOOST_AUTO_TEST_CASE(SingleDigit) {
-    BOOST_TEST(tools::intToString(5) == "5");
+TEST(StringTest, IntToStringTest_SingleDigit) {
+    EXPECT_EQ(tools::intToString(5), "5");
 }
 
-BOOST_AUTO_TEST_CASE(Zero) {
-    BOOST_TEST(tools::intToString(0) == "0");
+TEST(StringTest, IntToStringTest_Zero) {
+    EXPECT_EQ(tools::intToString(0), "0");
 }
 
-BOOST_AUTO_TEST_CASE(MultipleDigits) {
-    BOOST_TEST(tools::intToString(23523) == "23523");
+TEST(StringTest, IntToStringTest_MultipleDigits) {
+    EXPECT_EQ(tools::intToString(23523), "23523");
 }
 
-BOOST_AUTO_TEST_CASE(NegativeNumber) {
-    BOOST_TEST(tools::intToString(-125) == "-125");
+TEST(StringTest, IntToStringTest_NegativeNumber) {
+    EXPECT_EQ(tools::intToString(-125), "-125");
 }
 
-BOOST_AUTO_TEST_CASE(Hex) {
-    BOOST_TEST(tools::intToString(0x12dead, 16) == "12DEAD");
+TEST(StringTest, IntToStringTest_Hex) {
+    EXPECT_EQ(tools::intToString(0x12dead, 16), "12DEAD");
 }
 
-BOOST_AUTO_TEST_CASE(Oct) {
-    BOOST_TEST(tools::intToString(0137235, 8) == "137235");
+TEST(StringTest, IntToStringTest_Oct) {
+    EXPECT_EQ(tools::intToString(0137235, 8), "137235");
 }
 
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE(FloatToStringTest)
-
-BOOST_AUTO_TEST_CASE(Integer) {
-    BOOST_TEST(tools::floatToString(42.0, 2) == "42");
+TEST(StringTest, FloatToStringTest_Integer) {
+    EXPECT_EQ(tools::floatToString(42.0, 2), "42");
 }
 
-BOOST_AUTO_TEST_CASE(StopAtPoints) {
-    BOOST_TEST(tools::floatToString(51.34123, 2) == "51.34");
+TEST(StringTest, FloatToStringTest_StopAtPoints) {
+    EXPECT_EQ(tools::floatToString(51.34123, 2), "51.34");
 }
 
-BOOST_AUTO_TEST_CASE(Negative) {
-    BOOST_TEST(tools::floatToString(-42.6101, 2) == "-42.61");
+TEST(StringTest, FloatToStringTest_Negative) {
+    EXPECT_EQ(tools::floatToString(-42.6101, 2), "-42.61");
 }
 
-BOOST_AUTO_TEST_CASE(SmallPositive) {
-    BOOST_TEST(tools::floatToString(0.123, 2) == "0.12");
+TEST(StringTest, FloatToStringTest_SmallPositive) {
+    EXPECT_EQ(tools::floatToString(0.123, 2), "0.12");
 }
 
-BOOST_AUTO_TEST_CASE(SmallNegative) {
-    BOOST_TEST(tools::floatToString(-0.321, 2) == "-0.32");
+TEST(StringTest, FloatToStringTest_SmallNegative) {
+    EXPECT_EQ(tools::floatToString(-0.321, 2), "-0.32");
 }
 
-BOOST_AUTO_TEST_CASE(VerySmallPositive) {
-    BOOST_TEST(tools::floatToString(0.0001, 2) == "0.00");
+TEST(StringTest, FloatToStringTest_VerySmallPositive) {
+    EXPECT_EQ(tools::floatToString(0.0001, 2), "0.00");
 }
 
-BOOST_AUTO_TEST_CASE(VerySmallNegative) {
-    BOOST_TEST(tools::floatToString(-0.0001, 2) == "-0.00");
+TEST(StringTest, FloatToStringTest_VerySmallNegative) {
+    EXPECT_EQ(tools::floatToString(-0.0001, 2), "-0.00");
 }
 
-BOOST_AUTO_TEST_CASE(MoreComplicated) {
-    BOOST_TEST(tools::floatToString(1.0 / 3.0, 6) == "0.333333");
+TEST(StringTest, FloatToStringTest_MoreComplicated) {
+    EXPECT_EQ(tools::floatToString(1.0 / 3.0, 6), "0.333333");
 }
-
-BOOST_AUTO_TEST_SUITE_END()
-
-BOOST_AUTO_TEST_SUITE_END()
