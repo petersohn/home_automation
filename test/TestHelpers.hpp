beabc82238fb326412403761c83076fc5f27e8c7
Peter Szabados <kangirigungi@gmail.com>
2026-06-09 20:28:15 +0200

    Fix EXPECT_COLLECTIONS_EQ predicate-formatter signature

--- test/TestHelpers.hpp
+++ test/TestHelpers.hpp
@@ -3,23 +3,22 @@
 
 #include <gtest/gtest.h>
 
-#include <algorithm>
-#include <ostream>
-#include <string>
+#include <cstddef>
 
 template <typename IterA, typename IterB>
 ::testing::AssertionResult CollectionsEqual(
-    IterA a_begin, IterA a_end, IterB b_begin, IterB b_end, const char* a_expr,
-    const char* b_expr) {
+    const char* a_begin_expr, const char* a_end_expr, const char* b_begin_expr,
+    const char* b_end_expr, IterA a_begin, IterA a_end, IterB b_begin,
+    IterB b_end) {
     auto a_it = a_begin;
     auto b_it = b_begin;
-    size_t index = 0;
+    std::size_t index = 0;
     while (a_it != a_end && b_it != b_end) {
         if (!(*a_it == *b_it)) {
             return ::testing::AssertionFailure()
-                   << "Collections differ at index " << index << ": " << a_expr
-                   << "[" << index << "] = " << *a_it << ", " << b_expr << "["
-                   << index << "] = " << *b_it;
+                   << "Collections differ at index " << index << ": "
+                   << a_begin_expr << "[" << index << "] = " << *a_it << ", "
+                   << b_begin_expr << "[" << index << "] = " << *b_it;
         }
         ++a_it;
         ++b_it;
@@ -27,13 +26,13 @@ template <typename IterA, typename IterB>
     }
     if (a_it != a_end) {
         return ::testing::AssertionFailure()
-               << a_expr << " has " << (index + 1)
-               << " extra elements starting at index " << index;
+               << a_begin_expr << " has extra elements starting at index "
+               << index;
     }
     if (b_it != b_end) {
         return ::testing::AssertionFailure()
-               << b_expr << " has " << (index + 1)
-               << " extra elements starting at index " << index;
+               << b_begin_expr << " has extra elements starting at index "
+               << index;
     }
     return ::testing::AssertionSuccess();
 }
